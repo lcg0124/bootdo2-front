4ed19ec9615260550ec052535fb56b0d94879b33
itshixun/lichunguang <p_7g9v83>
2019-09-18 17:24:14 +0800

    保存功能

---
+++ src/components/List.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-container>
+    <el-button>新增</el-button>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="对象名称"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="包路径"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
 
+import List from '@/components/List'
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List
     }
   ]
 })
